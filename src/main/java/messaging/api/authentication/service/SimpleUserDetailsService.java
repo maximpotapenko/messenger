b5bccc7ee70dec18a74f44cac193fb33b789974d
Maxim Potapenko <maximpotapenko2002@gmail.com>
2023-07-15 21:23:09 +0300

    cleanup

--- src/main/java/messaging/api/authentication/service/SimpleUserDetailsService.java
+++ src/main/java/messaging/api/authentication/service/SimpleUserDetailsService.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import messaging.api.util.exception.ResourceNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +14,7 @@ public class SimpleUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         return repository.findByUsername(username)
                 .map(UserDetailsImpl::new)
                 .orElseThrow(() -> new ResourceNotFoundException("User does not exist"));
